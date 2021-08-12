83539ae5d6914cd6759db70d2f0b3b727faffb9c
Kovtunenko Victor <kovtunenko.victor@gmail.com>
2021-08-12 18:58:49 +0300

    Delete test method in controller

--- src/main/java/ru/open/way4service/reportservice/controllers/ReportController.java
+++ src/main/java/ru/open/way4service/reportservice/controllers/ReportController.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,11 +30,6 @@ public class ReportController {
 
     @Autowired
     ReportLoaderService reportLoaderService;
-
-    @GetMapping
-    public void testLog() throws ReportServiceException {
-        throw new ReportServiceException();
-    }
 
     @PostMapping(value = "/report/execute/{reportId}", produces = "application/json")
     @Operation(summary = "Provides the ability to run a report", description = "Provides execute report. Report run in thread pool. Set reportId in service path and put in request body map of report properties")
